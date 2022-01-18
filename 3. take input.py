bb8c7b3474908af9e7cff7fd35d056733d6da5e3
Subhranshu Choudhury <63858190+subhranshuchoudhury@users.noreply.github.com>
2022-01-18 20:01:37 +0530

    Create 3. take input.py

---
+++ 3. take input.py
@@ -0,0 +1,12 @@
+#in python it always take input as string.
+
+#variable_name = input("string")
+name = input("Enter Your Name: ")
+print("your name is ",name)
+
+#take input as int() and print the sum.
+
+a = int(input("Enter 1st Number: "))
+b = int(input("Enter 2nd Number: "))
+c = a+b
+print("Sum is: ",c)

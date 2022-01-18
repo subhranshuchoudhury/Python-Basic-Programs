3b5d979da1b0f7807e5ce1b40bee9cf1e2b90348
Subhranshu Choudhury <63858190+subhranshuchoudhury@users.noreply.github.com>
2022-01-18 19:51:42 +0530

    Create 2. simple addition program.py

---
+++ 2. simple addition program.py
@@ -0,0 +1,8 @@
+#take two variables no need for declaring as int.
+#take another variable and save the sum of the previous two variable
+#then print the 3rd variable.
+
+a = 1
+b = 2
+c = a + b
+print(c)

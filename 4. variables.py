42639b4adf3e73f85c4f04a941d291016e76fbd3
Subhranshu Choudhury <63858190+subhranshuchoudhury@users.noreply.github.com>
2022-01-18 20:08:30 +0530

    Create 4. variables.py

---
+++ 4. variables.py
@@ -0,0 +1,10 @@
+#in python we dont have command for declaring variables.
+#RULES:
+#1. variable name should start with letter or underscore.
+#2. variable name contain only alpha-numeric characters. (A-z,0-9,and _)
+#3. chintu , Chintu, CHINTU are 3 different variables.
+
+a = 5 #it means a is integer.
+b = "hello world" #b is string. content in double quote declared as string.
+print(a)
+print(b)

8cab114bf4fb2c4b13bb4d4682168954900ff5fa
Subhranshu Choudhury <63858190+subhranshuchoudhury@users.noreply.github.com>
2022-01-19 20:31:10 +0530

    Create 9. if statement.py

---
+++ 9. if statement.py
@@ -0,0 +1,9 @@
+#check even or odd
+#check previous file: https://raw.githubusercontent.com/subhranshuchoudhury/Python-Basic-Programs/main/8.%20control%20statement.txt
+
+
+num = int(input("Enter Number: "))
+if num%2==0:
+  print("--> Number is even.")
+else:
+  print("--> Number is odd.")

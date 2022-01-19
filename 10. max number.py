638bfac2f4e5fcb768f07d80256ef66c1720501b
Subhranshu Choudhury <63858190+subhranshuchoudhury@users.noreply.github.com>
2022-01-19 20:36:07 +0530

    Create 10. max number.py

---
+++ 10. max number.py
@@ -0,0 +1,10 @@
+a = int(input("Enter Number: "))
+b = int(input("Enter Number: "))
+
+if a>b:
+  print("1st number is larger.")
+elif b>a:
+  print("2nd number is larger.")
+else:
+  print("Numbers are same.")
+  

6822f32ea6aef06f8e9c75003396613f69d617e1
Subhranshu Choudhury <63858190+subhranshuchoudhury@users.noreply.github.com>
2022-01-19 11:19:41 +0530

    Create 7. swap two value.py

---
+++ 7. swap two value.py
@@ -0,0 +1,12 @@
+#swap value without no new variable
+
+a = 1
+b = 3
+#you can also take input: a=int(input("Enter Number: "))
+
+a = a+b #a=4
+b = a-b #b=1
+a = a-b #a=3
+
+print("Value of a ",a)
+print("Value of b ",b)

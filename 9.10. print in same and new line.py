c3c2b61f44cc1f343d60b3723807327fe71e8099
Subhranshu Choudhury <63858190+subhranshuchoudhury@users.noreply.github.com>
2022-01-20 19:08:07 +0530

    Create 9.10. print in same and new line.py

---
+++ 9.10. print in same and new line.py
@@ -0,0 +1,14 @@
+print("ram")
+print("shyam")
+
+# output:
+# ram
+# shyam
+
+print("________") # this is for just differentiate.
+
+print("ram",end='')
+print("shyam",end='')
+
+#output:
+# ramshyam

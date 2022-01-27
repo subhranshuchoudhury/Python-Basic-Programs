85fcecd82e9167b0ba3b2c19a1d89a2657cd8511
Subhranshu Choudhury <63858190+subhranshuchoudhury@users.noreply.github.com>
2022-01-27 22:30:15 +0530

    Update B1.3 string formatting.py

--- B1.3 string formatting.py
+++ B1.3 string formatting.py
@@ -3,3 +3,9 @@
 name, age = input("Enter Name & Age: ").split()
 
 print("Your name is {}.\nYour age is {}.".format(name, age))
+
+# python version 3.6+
+# another method.
+
+print(f"Another method:\nYour name is {name}.\nYour age is {age}.")
+

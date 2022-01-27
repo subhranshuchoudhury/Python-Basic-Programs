87318416fed557dcbd89d4a80ceee161414637fe
Subhranshu Choudhury <63858190+subhranshuchoudhury@users.noreply.github.com>
2022-01-27 22:49:10 +0530

    Update B1.3 string formatting.py

--- B1.3 string formatting.py
+++ B1.3 string formatting.py
@@ -9,3 +9,9 @@ print("Your name is {}.\nYour age is {}.".format(name, age))
 
 print(f"Another method:\nYour name is {name}.\nYour age is {age}.")
 
+# string formatting number input and show the avg in output
+
+num1,num2,num3 = input("Enter Number with comma separated: ").split(",")
+
+print((int(num1)+int(num2)+int(num3))/3)
+

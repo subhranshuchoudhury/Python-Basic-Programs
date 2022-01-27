8c35ccd0a8274d42e0fb12f077dd81726ce872d9
Subhranshu Choudhury <63858190+subhranshuchoudhury@users.noreply.github.com>
2022-01-27 20:39:35 +0530

    Create B1.1 emoji print.py

---
+++ B1.1 emoji print.py
@@ -0,0 +1,6 @@
+# trick to print emojis.
+# open this site: https://unicode.org/emoji/charts/full-emoji-list.html and copy the unicode of selected emoji.
+# replace the '+' sign with '000' and '\'. eg. U+1F604 --->  \U0001F604
+
+#code-----
+print("EMOJI: \U0001F604") #output: EMOJI: 😄

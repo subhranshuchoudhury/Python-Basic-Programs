fa299bb67cb3eea9af3fc62f4385389fc1accc0f
Subhranshu Choudhury <63858190+subhranshuchoudhury@users.noreply.github.com>
2022-01-28 11:30:42 +0530

    Create B1.7 string method.py

---
+++ B1.7 string method.py
@@ -0,0 +1,32 @@
+# string method:
+
+word = 'Coding master.'
+
+# length -- len(variable)
+
+print(len(word))
+
+# upper()
+
+print(word.upper())
+
+# lower()
+
+print(word.lower())
+
+# title()
+
+print(word.title())
+
+# count()
+
+print(word.count("m"))  # count the m character in the word variable.
+
+
+# --- output ---
+
+# 14
+# CODING MASTER.
+# coding master.
+# Coding Master.
+# 1

1e48d5cea076e0a295bc2cd4fb81f5fc234700af
Subhranshu Choudhury <63858190+subhranshuchoudhury@users.noreply.github.com>
2022-01-28 11:04:32 +0530

    Create B1.5 string slicing.py

---
+++ B1.5 string slicing.py
@@ -0,0 +1,9 @@
+# string slicing:
+
+word = 'HelloWorld'
+
+# syntax: print(variable[start_index : stop_index-1])
+
+print(word[0:3])  # output: Hel
+print(word[:6])   # output: Hellow
+print(word[1:])   # output: elloWorld

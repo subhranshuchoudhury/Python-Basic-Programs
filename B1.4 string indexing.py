47faeacc1ccae9987dd8f76d7ea3744046dfebdc
Subhranshu Choudhury <63858190+subhranshuchoudhury@users.noreply.github.com>
2022-01-28 10:55:51 +0530

    Create B1.4 string indexing.py

---
+++ B1.4 string indexing.py
@@ -0,0 +1,22 @@
+# create a variable for storing our string.
+word = 'github'
+
+# index number - character
+
+# 0 -- g
+# 1 -- i
+# 2 -- t
+# 3 -- h
+# 4 -- u
+# 5 -- b
+
+# -1 -- b
+# -2 -- u
+# -3 -- h
+# -4 -- t
+# -5 -- i
+# -6 -- g
+
+print(word[-1])  # we always use [] square bracket for string index.
+
+# output: b

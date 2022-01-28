f438bb60e2ad35672650f7486808a83703f2e477
Subhranshu Choudhury <63858190+subhranshuchoudhury@users.noreply.github.com>
2022-01-28 11:12:32 +0530

    Create B1.6 step argument.py

---
+++ B1.6 step argument.py
@@ -0,0 +1,8 @@
+# step argument.
+
+word = 'HelloWorld'
+
+# syntax: print(variable[start_index : stop_index-1 : step])
+# step argument will skip letters to next one.
+# here the step argument is 2. it will skip one character. (skip character = step-1)
+print(word[0::2])  # output: Hlool
